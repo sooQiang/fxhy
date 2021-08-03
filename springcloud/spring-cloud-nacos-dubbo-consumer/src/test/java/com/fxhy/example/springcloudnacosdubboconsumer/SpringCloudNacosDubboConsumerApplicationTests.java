b33a27b8ce83268a2f494746545b2c1df52e91fc
shuqiang <shuqiang@yuantutech.com>
2021-08-03 17:35:30 +0800

    nacos+dubbo+注册中心

---
+++ springcloud/spring-cloud-nacos-dubbo-consumer/src/test/java/com/fxhy/example/springcloudnacosdubboconsumer/SpringCloudNacosDubboConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fxhy.example.springcloudnacosdubboconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudNacosDubboConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
